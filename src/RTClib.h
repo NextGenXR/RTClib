82fd71526c579686300eeb23e92be5c837ceb971
Jenni O'Connor <jenni@nextgenxr.com>
2022-11-22 13:15:07 -0600

    Fix comment //

--- src/RTClib.h
+++ src/RTClib.h
@@ -46,7 +46,7 @@ class TimeSpan;
 /** Constants */
 #define SECONDS_PER_DAY 86400L ///< 60 * 60 * 24
 #define SECONDS_FROM_1970_TO_2000                                              \
-  946684800 ///< Unixtime for 2000-01-01 00:00:00, useful for initialization
+  946684800 /* /< Unixtime for 2000-01-01 00:00:00, useful for initialization */
 
 /** Alarm modes for STM32 alarm 1 */
 enum STM32Alarm1Mode {
